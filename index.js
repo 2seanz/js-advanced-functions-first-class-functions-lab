d689e4f68954145f079b3f8c82ce2eef7a8ea027
Sean Ngo <seanzngo@gmail.com>
2021-04-06 17:22:08 -0400

    commit tree 3eb25e1e074e1e01a4e8544822393f79a4776c43

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file!
+const driver = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+
+function returnFirstTwoDrivers(myArr) {
+    let newArray = myArr.slice(0,2)
+    return newArray
+}
+
+function returnLastTwoDrivers(myArr) {
+    let newArray = myArr.slice(myArr.length-2, myArr.length)
+    return newArray
+}
